f3dc58fc335c700b1360ddc91c2d5a2a9521c3c4
Viktor Nagy <viktor.nagy@365.bank>
2024-01-05 15:35:26 +0100

    feat: not found on edit page

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,12 +1,24 @@
 import { currentUser } from "@clerk/nextjs";
+import { api } from "~/trpc/server";
+import { notFound } from "next/navigation";
 
-export default async function Edit({ params }: { params: { id: string } }) {
+export default async function Edit({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   const user = await currentUser();
   if (!user) return <div>Not logged in</div>;
 
+  const record = await api.record.getById.query({
+    userId: user.id,
+    id: parseInt(id),
+  });
+  if (!record) return notFound();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      {params.id}
+      {record.id}
     </main>
   );
 }
